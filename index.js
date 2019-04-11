3033ac90e94d5a3c45a66c026925fbf830c23f5b
polynomiald <steven.grindey@gmail.com>
2019-04-12 00:47:47 +0100

    menu labels + cross platform checks

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ app.on('ready', () => {
 
 const menuTemplate = [
   {
-    label: 'file'
+    label: 'file',
+    submenu: [
+      { label: 'New Todo'},
+      {
+        label:'quit',
+        accelerator: (() => {
+         if (process.platform === 'darwin') {
+           return 'Command+Q'
+         } else {
+           return 'Ctrl+Q'
+         }
+        })(),
+        click() {
+          app.quit()
+        }
+      }
+    ]
   }
 ]
+  // Mac check
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({})
+}
