02664980d6e9f0e9a52837d37f6f64bb0a8472d7
polynomiald <steven.grindey@gmail.com>
2019-04-18 22:32:44 +0100

    addWindow ipc and garbage collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron')
 
-const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow, Menu, ipcMain } = electron
 
 let mainWindow, addWindow
 
@@ -20,7 +20,13 @@ function createAddWindow() {
     title: 'Add New Todo'
   })
   addWindow.loadURL(`file://${__dirname}/add.html`)
+  addWindow.on('closed', () => addWindow = null)
 }
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo)
+  addWindow.close()
+})
 
 const menuTemplate = [
   {
